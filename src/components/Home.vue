5aa649303184cb2f99423f6f193d5cbe6aa3661c
Vinh Peters <vinhpeters@outlook.com>
2022-09-19 20:06:57 -0400

    Remove entry from home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,17 @@
 <template>
-  <Header/>
-  <Carousel/>
+  <Header />
+  <Carousel />
 </template>
 
 <script>
-import Jumbotron from "./Jumbotron.vue";
 import NavBar from "./NavBar.vue";
-import Entry from "./Entry.vue";
 import Header from "./Header.vue";
-import  Carousel  from "./Carousel.vue";
+import Carousel from "./Carousel.vue";
 
 export default {
   name: "Home",
   components: {
-    Jumbotron,
     NavBar,
-    Entry,
     Header,
     Carousel,
   },
